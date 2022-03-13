e1da9531af22c7c74406405c2e127cef9bb82c38
Andrey Podvysotskiy <podvysotskiy@gmail.com>
2022-03-13 16:20:11 -0700

    added event listener for filtering generated table

--- static/js/app.js
+++ static/js/app.js
@@ -20,4 +20,28 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").propery("value");
+    let filteredData = tableData;
+    
+    // Check to see if a date was entered and filter the
+    // data using that date
+    if (date) {
+        // Apply the 'filter' to the table data to only keep the rows
+        // where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event listener to the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
